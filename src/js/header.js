f65c667202632e7adaf32d4e3665fc5edf80e370
1Rendes <1admin@ukr.net>
2024-10-05 22:57:31 +0200

    noscroll fix

--- src/js/header.js
+++ src/js/header.js
@@ -1,8 +1,6 @@
 const header = document.querySelector('.header');
 
 window.addEventListener('scroll', () => {
-  console.log(window.scrollY);
-
   if (window.scrollY) {
     header.classList.add('scrolled');
   } else {
